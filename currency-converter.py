9fd4b2768f7adbec5f4ac447618c95bec3035250
Vinz2008 <68145293+Vinz2008@users.noreply.github.com>
2021-08-09 01:08:49 +0200

    Create currency-converter.py

---
+++ currency-converter.py
@@ -0,0 +1,10 @@
+import tkinter as tk
+from tkinter import *
+import tkinter.messagebox
+root = tk.Tk()
+root.title("CurrencyConvertorPy")
+Tops = Frame(root, bg='#e6e5e5', pady=2, width=1850, height=100, relief="ridge")
+Tops.grid(row=0, column=0)
+headlabel = tk.Label(Tops, font=('lato black', 19, 'bold'), text='CurrencyConverterPy'bg='#e6e5e5', fg='black')
+headlabel.grid(row=1, column=0, sticky=W)
+
